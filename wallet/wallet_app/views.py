38c096da9eed4c9d8ec7c129c128f6ad0068de29
Andrey <andmaster.one@gmail.com>
2021-03-22 23:46:43 +0300

    Swagger bug masked

--- wallet/wallet_app/views.py
+++ wallet/wallet_app/views.py
@@ -7,12 +7,18 @@ class TransactionViewSet(viewsets.ModelViewSet):
     serializer_class = TransactionSerialize
 
     def get_queryset(self):
+        if getattr(self, 'swagger_fake_view', False):
+            # queryset just for schema generation metadata
+            return Transaction.objects.none()
         return Transaction.objects.filter(wallet_id = self.kwargs['wallet_pk'])
 
 class WalletViewSet(viewsets.ModelViewSet):
     serializer_class = WalletSerializer
 
     def get_queryset(self):
+        if getattr(self, 'swagger_fake_view', False):
+            # queryset just for schema generation metadata
+            return Wallet.objects.none()
         return Wallet.objects.filter(user_id = self.kwargs['user_pk'])
 
 class UserViewSet(mixins.RetrieveModelMixin,
